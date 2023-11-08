8dfea6b8324dd578c30dcfae52d006c8ef4c7c97
Joshardals <joshuabamidele219@gmail.com>
2023-11-08 22:37:44 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="relative">
       <Header />
-      <div className="min-h-screen h-[calc(100vh - 250px)] bg-green w-full"></div>
+      <div className="min-h-screen w-full"></div>
     </main>
   );
 }
