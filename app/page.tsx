fde510022efbff3b637ca108e8ba9894f91d6300
Joshardals <joshuabamidele219@gmail.com>
2023-11-08 19:19:49 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/LandingPage/Header/Header";
 
 export default function Home() {
   return (
-    <main className="">
+    <main className="bg-white">
       <Header />
       <div className="h-screen w-full">hey there</div>
     </main>
