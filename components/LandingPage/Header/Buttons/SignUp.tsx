20218d6bdffdeef3dc254df58c9122a51f234703
Joshardals <joshuabamidele219@gmail.com>
2023-11-10 00:15:58 +0100

    initial commit

--- components/LandingPage/Header/Buttons/SignUp.tsx
+++ components/LandingPage/Header/Buttons/SignUp.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const SignUp = () => {
   return (
     <div>
-      <button className="h-12 w-auto bg-gold text-green max-md:mr-10 px-8 text-center rounded-full">
+      <button className="h-12 w-auto bg-gold text-green max-md:mr-5 px-8 text-center rounded-full">
         Sign up
       </button>
     </div>
