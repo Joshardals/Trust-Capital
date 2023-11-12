ba940fa726d78fd911c5a2ef63a33ee198a3d364
Joshardals <joshuabamidele219@gmail.com>
2023-11-12 19:15:03 +0100

    initial commit

--- components/LandingPage/HeroSection/HeroSection.tsx
+++ components/LandingPage/HeroSection/HeroSection.tsx
@@ -3,13 +3,11 @@ import Image from "next/image";
 const HeroSection = () => {
   return (
     <div
-      className=" mt-40 max-md:px-5 md:px-20 lg:px-40 max-md:space-y-4 md:space-x-8 flex items-center md:flex-row flex-col
+      className=" max-md:mt-20 md:mt-40 max-md:px-5 md:px-20 lg:px-40 max-md:space-y-4 md:space-x-8 flex items-center md:flex-row flex-col
     "
     >
       <div className=" text-pureblack md:max-w-md ">
-        <h1 className=" text-xl md:text-2xl leading-tight">
-          Empower Your Wealth Journey
-        </h1>
+        <h1 className="text-2xl leading-tight">Empower Your Wealth Journey</h1>
         <p className="font-sans">
           Where trust meets opportunity. Trust Capital Investment is your
           partner in financial growth. Secure your future with confidence,
