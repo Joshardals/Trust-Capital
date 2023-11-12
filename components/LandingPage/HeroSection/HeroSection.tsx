1db67b73d3cdaf10d3a4e93a7e95c75deafb4263
Joshardals <joshuabamidele219@gmail.com>
2023-11-12 19:07:12 +0100

    initial commit

--- components/LandingPage/HeroSection/HeroSection.tsx
+++ components/LandingPage/HeroSection/HeroSection.tsx
@@ -1,27 +1,29 @@
-import React from "react";
 import Image from "next/image";
 
 const HeroSection = () => {
   return (
     <div
-      className=" mt-40 max-md:px-5 md:px-20 lg:px-40 max-md:space-y-4 lg:space-x-4 flex items-center md:flex-row flex-col
+      className=" mt-40 max-md:px-5 md:px-20 lg:px-40 max-md:space-y-4 md:space-x-8 flex items-center md:flex-row flex-col
     "
     >
-      <div className="h-auto w-full flex-1">
-        <h1 className="text-2xl leading-tight">
-          Grow Your Wealth <br /> With Us
+      <div className=" text-pureblack md:max-w-md p-5">
+        <h1 className=" text-xl md:text-2xl leading-tight">
+          Empower Your Wealth Journey
         </h1>
-        <p className="font-sans block">
-          Invest with confidence. Trust Capital Investment is your gateway to
-          financial success.
+        <p className="font-sans">
+          Where trust meets opportunity. Trust Capital Investment is your
+          partner in financial growth. Secure your future with confidence,
+          backed by a legacy of trustworthiness. Start your investment journey
+          today.
         </p>
       </div>
-      <div className="h-full w-full bg-babyblue flex-1">
+
+      <div className="h-full w-full">
         <Image
           src="/hero-img.png"
           width={1600}
           height={800}
-          className="w-full max-sm:h-60 max-md:h-72"
+          className="w-full max-sm:h-[500x] max-md:h-72"
           alt="Hero-Image"
         />
       </div>
