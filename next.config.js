d176856ba61dc620b2ed122313cbbb5887b597e1
Joshardals <joshuabamidele219@gmail.com>
2023-11-22 16:23:40 +0100

    initial commit

--- next.config.js
+++ next.config.js
@@ -1,5 +1,31 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async headers() {
+    const commonHeaders = [
+      {
+        key: 'Content-Security-Policy',
+        value: 'upgrade-insecure-requests',
+      },
+      {
+        key: 'viewport',
+        value: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0',
+      },
+      // add other common headers if needed
+    ];
+  
+    return [
+      {
+        source: '/signup',
+        headers: commonHeaders,
+      },
+      {
+        source: '/login',
+        headers: commonHeaders,
+      },
+      // add more routes as needed
+    ];
+  },
+  
     images: {
         remotePatterns: [
           {
