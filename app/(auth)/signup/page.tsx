01f237c9d20b7c944a97f58eba31841a6ab59fa3
Joshardals <joshuabamidele219@gmail.com>
2023-11-22 20:09:46 +0100

    initial commit

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -72,7 +72,7 @@ export default function Page() {
             <div className="absolute top-0 left-0 h-full w-full bg-navyblue/70" />
           </div>
 
-          <div className="p-5 bg-babyblue overflow-auto h-full">
+          <div className="p-5 bg-babyblue h-full">
             <UserAuthForm />
           </div>
         </div>
