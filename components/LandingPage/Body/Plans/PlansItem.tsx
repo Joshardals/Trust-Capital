390bb6a519830dc0d78bbc6a2b26963d24006938
Joshardals <joshuabamidele219@gmail.com>
2023-11-27 18:15:12 +0100

    initial commit

--- components/LandingPage/Body/Plans/PlansItem.tsx
+++ components/LandingPage/Body/Plans/PlansItem.tsx
@@ -5,31 +5,31 @@ const PlansItem = ({ plan, percentage, term, minMax, referral }: PlanProps) => {
   return (
     <div className="p-5 md:py-10 md:px-5 shadow-lg shadow-navyblue/40 flex flex-col space-y-6 rounded-2xl select-none">
       <div className="flex flex-col items-center space-y-2">
-        <Icons.shield className="w-10 h-10" />
+        <Icons.shield className="w-10 h-10 text-puregreen" />
         <p className=" uppercase">{plan}</p>
         <h1 className="text-2xl font-semibold">{percentage}%</h1>
       </div>
       <div className="space-y-4">
         <div className="flex items-center space-x-4">
-          <Icons.check className="h-4 w-4" />
+          <Icons.check className="h-4 w-4 text-puregreen" />
           <p>
             Plan Terms: <span className="font-semibold">{term} Hours</span>
           </p>
         </div>
         <div className="flex items-center space-x-4">
-          <Icons.check className="h-4 w-4" />
+          <Icons.check className="h-4 w-4 text-puregreen" />
           <p>
             Minimum/Maximum: <span className="font-semibold">{minMax}</span>
           </p>
         </div>
         <div className="flex items-center space-x-4">
-          <Icons.check className="h-4 w-4" />
+          <Icons.check className="h-4 w-4 text-puregreen" />
           <p>
             Withdrawal: <span className="font-semibold">Instant</span>
           </p>
         </div>
         <div className="flex items-center space-x-4">
-          <Icons.check className="h-4 w-4" />
+          <Icons.check className="h-4 w-4 text-puregreen" />
           <p>
             Referral Commision:{" "}
             <span className="font-semibold">{referral}%</span>
