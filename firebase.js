1370d882cc4a3cea778d263ec53a6531a63ac62a
Joshardals <joshuabamidele219@gmail.com>
2023-12-07 20:44:27 +0100

    initial commit

--- firebase.js
+++ firebase.js
@@ -1,25 +1,21 @@
-// Import the functions you need from the SDKs you need
+// firebase.js
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA52ne81iv1deLF7ZgvTweu-bYcNkQb1u8",
-  authDomain: "trust-capital-c9a82.firebaseapp.com",
-  projectId: "trust-capital-c9a82",
-  storageBucket: "trust-capital-c9a82.appspot.com",
-  messagingSenderId: "776262696587",
-  appId: "1:776262696587:web:14557ab31346f9f4ac1e8e",
-  measurementId: "G-SB52CH0XCN"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-const auth = getAuth(app) 
+const auth = getAuth(app);
 
-export { app, auth}
+export { app, auth };
