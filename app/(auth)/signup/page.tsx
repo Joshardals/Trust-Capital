1d5a8a041f4ea56979841a7b58851cec7f641b8e
Joshardals <joshuabamidele219@gmail.com>
2023-12-30 14:07:02 +0100

    initial commit

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -6,8 +6,7 @@ export const metadata: Metadata = {
   title: "Create your login | Trust-Capital Investment",
   description: "Create your login for Trust-Capital",
   openGraph: {
-    images:
-      "https://images.deepai.org/machine-learning-models/0c7ba850aa2443d7b40f9a45d9c86d3f/text2imgthumb.jpeg",
+    images: "https://ibb.co/0fVZqdV",
   },
 };
 
