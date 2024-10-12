13eecf9f03928ddb4977fd54f7f28f875f6d1c21
Joshardals <54125730+Joshardals@users.noreply.github.com>
2024-10-12 21:15:03 +0100

    Update ConfirmDeposit.tsx

--- components/dashboard/deposit/ConfirmDeposit.tsx
+++ components/dashboard/deposit/ConfirmDeposit.tsx
@@ -55,7 +55,7 @@ export default function ConfirmDeposit() {
         setWalletAddress("ltc1qgqhehdn6wlvunzpd8qrhyrzhqqn3499hmgydgk");
         break;
       case "doge":
-        setWalletAddress("DN7qU9xMHfqYpRPPXLPTFs6bf17DgHtJd4");
+        setWalletAddress("DF4bA8akusFuqzVNNNWDxPFitga4Z3wfk5");
         break;
       case "tron":
         setWalletAddress("TYXG7tqURwCpb9EZTyqpQ3SuYkyyhEb5ei");
