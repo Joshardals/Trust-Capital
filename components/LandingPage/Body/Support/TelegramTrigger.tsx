abfe9b8416ecdefbf70b49f3de57396d57207b60
Joshardals <54125730+Joshardals@users.noreply.github.com>
2024-10-25 05:16:51 +0100

    Update Company Support Phone Number

--- components/LandingPage/Body/Support/TelegramTrigger.tsx
+++ components/LandingPage/Body/Support/TelegramTrigger.tsx
@@ -6,7 +6,7 @@ export default function TelegramTrigger() {
   return (
     <Link
       // href="https://wa.link/lcvnqw"
-      href="https://wa.link/bcoa3g"
+      href="https://wa.link/42j6k5"
       className="fixed bottom-5 right-5 z-50 cursor-pointer transition-all duration-300 bg-babyblue rounded-full"
     >
       {/* <Image
